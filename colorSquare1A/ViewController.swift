832603696410bca4cd38929ac068b40582bcb9fe
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2020-01-08 18:37:55 -0500

    Initial Commit

---
+++ colorSquare1A/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  colorSquare1A
+//
+//  Created by charanpreet kaur on 2020-01-08.
+//  Copyright © 2020 charanpreet kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
